0e4decc7008c801ae75abbb5235010f6ebe23c7d
viebke <andre@viebke87.se>
2014-11-05 21:01:18 +0100

    RED

---
+++ SimpleInventorySystem/tests/inventory/TestArticle.java
@@ -0,0 +1,24 @@
+package inventory;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestArticle {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void shouldCreateNewInstance() {
+		new Article();
+	}
+
+}
