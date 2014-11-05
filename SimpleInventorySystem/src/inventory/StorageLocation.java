28e14718c60eba1d6d2bb88fb5921084fd7bb3c1
viebke <andre@viebke87.se>
2014-11-05 21:46:32 +0100

    GREEN

---
+++ SimpleInventorySystem/src/inventory/StorageLocation.java
@@ -0,0 +1,11 @@
+package inventory;
+
+public class StorageLocation {
+
+	public StorageLocation(Object object) {
+		// TODO Auto-generated constructor stub
+		if(null == object)
+			throw new IllegalArgumentException();
+	}
+
+}
