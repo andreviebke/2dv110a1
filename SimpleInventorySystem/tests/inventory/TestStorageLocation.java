dea3e5123ee624bd27a55b4c4712479f37540ca9
viebke <andre@viebke87.se>
2014-11-05 21:59:40 +0100

    REFACTOR

--- SimpleInventorySystem/tests/inventory/TestStorageLocation.java
+++ SimpleInventorySystem/tests/inventory/TestStorageLocation.java
@@ -1,16 +1,17 @@
 package inventory;
 
-import java.util.LinkedList;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
 public class TestStorageLocation {
+
+	private static String STORAGE_NAME = "MyStorageLocation";
 
 	@Before
 	public void setUp() throws Exception {
@@ -24,26 +25,26 @@ public class TestStorageLocation {
 	public void shouldThrowOnNull() {
 		new StorageLocation(null);
 	}
-	
+
 	@Test
-	public void shouldCreateInstanceWithName()
-	{
-		StorageLocation s = new StorageLocation("MyStorageLocation");
-		assertEquals(s.getName(), "MyStorageLocation");
+	public void shouldCreateInstanceWithName() {
+		StorageLocation s = new StorageLocation(
+				TestStorageLocation.STORAGE_NAME);
+		assertEquals(s.getName(), TestStorageLocation.STORAGE_NAME);
 	}
-	
+
 	@Test
-	public void shouldCreateInstanceWithASetOfArticles()
-	{
+	public void shouldCreateInstanceWithASetOfArticles() {
 		@SuppressWarnings("unchecked")
 		List<Article> input = mock(List.class);
-		
+
 		Article a1 = mock(Article.class);
-		Article a2 = mock(Article.class);		
+		Article a2 = mock(Article.class);
 		input.add(a1);
 		input.add(a2);
-		
-		StorageLocation s = new StorageLocation("MyStorageLocation", input);
+
+		StorageLocation s = new StorageLocation(
+				TestStorageLocation.STORAGE_NAME, input);
 		assertEquals(input, s.getArticles());
 	}
 
