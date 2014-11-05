bae92e0fe0c91bc0dab0a5af124c25b39c6012d8
viebke <andre@viebke87.se>
2014-11-05 21:45:51 +0100

    RED

---
+++ SimpleInventorySystem/tests/inventory/TestStorageLocation.java
@@ -0,0 +1,24 @@
+package inventory;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestStorageLocation {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldThrowOnNull() {
+		new StorageLocation(null);
+	}
+
+}
