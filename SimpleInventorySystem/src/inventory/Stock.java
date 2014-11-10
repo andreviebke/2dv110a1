4f8761718d56e0a82661a4e40d427c134848acf0
viebke <andre@viebke87.se>
2014-11-10 10:17:27 +0100

    GREEN

--- SimpleInventorySystem/src/inventory/Stock.java
+++ SimpleInventorySystem/src/inventory/Stock.java
@@ -1,20 +1,40 @@
 package inventory;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Stock {
 
 	private double temperature;
+	private List<StorageLocation> storageLocations;
 	public static double MAX_TEMP = 30;
 	public static double MIN_TEMP = 20;
+	
+	public Stock()
+	{
+		this.storageLocations = new LinkedList<StorageLocation>();
+	}
 	
 	public void setTemperature(double d) {
 		if(d < Stock.MIN_TEMP || d > Stock.MAX_TEMP)
 			throw new IllegalArgumentException();
 		
-		this.temperature = d;
+		this.temperature = d;		
 	}
 
 	public Object getTemperature() {
 		return this.temperature;
+	}
+
+	public List<StorageLocation> getStorageLocations() {
+		return this.storageLocations;
+	}
+
+	public void addStorageLocation(StorageLocation loc) {
+		if(null == loc)
+			throw new IllegalArgumentException();
+		
+		this.storageLocations.add(loc);
 	}
 
 }
--- SimpleInventorySystem/tests/inventory/TestStock.java
+++ SimpleInventorySystem/tests/inventory/TestStock.java
@@ -2,7 +2,8 @@ package inventory;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
