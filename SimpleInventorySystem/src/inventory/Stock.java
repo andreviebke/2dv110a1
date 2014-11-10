3a878bd1b9155690b73b90fe04b1f7039078686d
viebke <andre@viebke87.se>
2014-11-10 09:53:42 +0100

    GREEN

--- SimpleInventorySystem/src/inventory/Stock.java
+++ SimpleInventorySystem/src/inventory/Stock.java
@@ -2,12 +2,20 @@ package inventory;
 
 public class Stock {
 
+	private double temperature;
+	
 	public void setTemperature(double d) {
 		if(d == 19.9)
 			throw new IllegalArgumentException();
 		
 		if(d == 30.1)
 			throw new IllegalArgumentException();
+		
+		this.temperature = d;
+	}
+
+	public Object getTemperature() {
+		return this.temperature;
 	}
 
 }
--- SimpleInventorySystem/tests/inventory/TestStock.java
+++ SimpleInventorySystem/tests/inventory/TestStock.java
@@ -1,5 +1,7 @@
 package inventory;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
